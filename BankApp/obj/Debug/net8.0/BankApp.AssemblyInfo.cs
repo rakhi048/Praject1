3b101041f4c74f74a9e3cdb7342a1befb78e3ab0
rakhi048 <rakeshpachunoori143@gmail.com>
2024-02-23 11:12:01 +0000

    update

--- BankApp/obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ BankApp/obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b44593fa7afdf2ec189d2138273739288b2143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc702de673a3c4bf0526175b35474dca3d82fde")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
